a14a450b53eab89ee30738b63fd981cdc4cb180f
Cynosure-Null <cynosure@disroot.org>
2023-09-09 15:11:53 -0600

    fixed state issue and formatting

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -7,7 +7,8 @@
 #include <fmt/core.h>
 #include <frc/smartdashboard/SmartDashboard.h>
 
-void Robot::RobotInit() {
+void Robot::RobotInit()
+{
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
@@ -26,90 +27,118 @@ void Robot::RobotPeriodic() {}
 /**
  * This autonomous (along with the chooser code above) shows how to select
  * between different autonomous modes using the dashboard. The sendable chooser
- * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
- * remove all of the chooser code and uncomment the GetString line to get the
- * auto name from the text box below the Gyro.
+ * code works with the Java SmartDashboard. If you prefer the LabVIEW
+ * Dashboard, remove all of the chooser code and uncomment the GetString line
+ * to get the auto name from the text box below the Gyro.
  *
  * You can add additional auto modes by adding additional comparisons to the
- * if-else structure below with additional strings. If using the SendableChooser
- * make sure to add them to the chooser code above as well.
+ * if-else structure below with additional strings. If using the
+ * SendableChooser make sure to add them to the chooser code above as well.
  */
-void Robot::AutonomousInit() {
+void Robot::AutonomousInit()
+{
   m_autoSelected = m_chooser.GetSelected();
   // m_autoSelected = SmartDashboard::GetString("Auto Selector",
   //     kAutoNameDefault);
   fmt::print("Auto selected: {}\n", m_autoSelected);
 
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
+  if(m_autoSelected == kAutoNameCustom)
+    {
+      // Custom Auto goes here
+    }
+  else
+    {
+      // Default Auto goes here
+    }
 }
 
-void Robot::AutonomousPeriodic() {
-  if (m_autoSelected == kAutoNameCustom) {
-    // Custom Auto goes here
-  } else {
-    // Default Auto goes here
-  }
+void Robot::AutonomousPeriodic()
+{
+  if(m_autoSelected == kAutoNameCustom)
+    {
+      // Custom Auto goes here
+    }
+  else
+    {
+      // Default Auto goes here
+    }
 }
 
 void Robot::TeleopInit() {}
 
-void Robot::TeleopPeriodic() {
-  if (BUTTON::STOWED())
-  {
-    m_stowed_toggle = !m_stowed_toggle;
-    m_intake_toggle = 0;
-    m_man_intake_toggle = 0;
-    m_extake_toggle = 0;
-  }
-   if (BUTTON::INTAKE())
-  {
-    m_stowed_toggle = 0;
-    m_intake_toggle = !m_intake_toggle;
-    m_man_intake_toggle = 0;
-    m_extake_toggle = 0;
-  }
-   if (BUTTON::MAN_INTAKE())
-  {
-    m_stowed_toggle = 0;
-    m_intake_toggle = 0;
-    m_man_intake_toggle = !m_man_intake_toggle;
-    m_extake_toggle = 0;
-  }
-   if (BUTTON::EXTAKE())
-  {
-    m_stowed_toggle = 0;
-    m_intake_toggle = 0;
-    m_man_intake_toggle = 0;
-    m_extake_toggle = !m_extake_toggle;
-  }
-  switch (m_state)
-  {
+void Robot::TeleopPeriodic()
+{
+  if(BUTTON::STOWED())
+    {
+      m_stowed_toggle = !m_stowed_toggle;
+      m_intake_toggle = 0;
+      m_man_intake_toggle = 0;
+      m_extake_toggle = 0;
+    }
+  if(BUTTON::INTAKE())
+    {
+      m_stowed_toggle = 0;
+      m_intake_toggle = !m_intake_toggle;
+      m_man_intake_toggle = 0;
+      m_extake_toggle = 0;
+    }
+  if(BUTTON::MAN_INTAKE())
+    {
+      m_stowed_toggle = 0;
+      m_intake_toggle = 0;
+      m_man_intake_toggle = !m_man_intake_toggle;
+      m_extake_toggle = 0;
+    }
+  if(BUTTON::EXTAKE())
+    {
+      m_stowed_toggle = 0;
+      m_intake_toggle = 0;
+      m_man_intake_toggle = 0;
+      m_extake_toggle = !m_extake_toggle;
+    }
+
+  if(m_extake_toggle)
+    {
+      m_state = CONSTANTS::STATE::EXTAKE;
+    }
+  if(m_intake_toggle)
+    {
+      m_state = CONSTANTS::STATE::INTAKE;
+    }
+  if(m_stowed_toggle)
+    {
+      m_state = CONSTANTS::STATE::STOWED;
+    }
+  if(m_man_intake_toggle)
+    {
+      m_state = CONSTANTS::STATE::MAN_INTAKE;
+    }
+  switch(m_state)
+    {
     case CONSTANTS::STATE::STOWED:
       m_arm.move(CONSTANTS::ARM::STORE_POS);
       m_roller.spin(CONSTANTS::ROLLER::SLOW);
 
       break;
-    case CONSTANTS::STATE::MAN_INTAKE: //NOTE: No specific code needs to be written for man intake. If the state is man intake, is_loaded will always return false.
-      //Fallthough inferred
+    case CONSTANTS::STATE::MAN_INTAKE: // NOTE: No specific code needs to be
+                                       // written for man intake. If the state
+                                       // is man intake, is_loaded will always
+                                       // return false. Fallthough inferred
     case CONSTANTS::STATE::INTAKE:
       m_arm.move(CONSTANTS::ARM::INTAKE_POS);
       m_roller.spin(1);
-    
-      if (m_roller.is_loaded())
-      {
-        m_state = CONSTANTS::STATE::STOWED;
-      }
+
+      if(m_roller.is_loaded())
+        {
+          m_state = CONSTANTS::STATE::STOWED;
+        }
       break;
-    case CONSTANTS::STATE::EXTAKE: 
+    case CONSTANTS::STATE::EXTAKE:
       m_arm.move(CONSTANTS::ARM::SCORE_POS);
       m_roller.spin(CONSTANTS::FF_SPEED);
       m_roller.spin(0);
       break;
-  }
+    }
 }
 void Robot::DisabledInit() {}
 
@@ -124,7 +153,5 @@ void Robot::SimulationInit() {}
 void Robot::SimulationPeriodic() {}
 
 #ifndef RUNNING_FRC_TESTS
-int main() {
-  return frc::StartRobot<Robot>();
-}
+int main() { return frc::StartRobot<Robot>(); }
 #endif
