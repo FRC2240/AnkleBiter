a144f299ee647deee0c63b7d7b4630e644efe4e5
Cynosure-Null <cynosure@disroot.org>
2023-09-13 15:06:58 -0600

    Rebased
    -- Commit made by Jeff Bezos Stonerbot
    (AKA cynosure or Cynosure-NUL) <cynosure@disroot.org>
    My PGP key can be found on my github profile
    Fly safe

--- src/main/cpp/Robot.cpp
+++ src/main/cpp/Robot.cpp
@@ -12,6 +12,10 @@ void Robot::RobotInit()
   m_chooser.SetDefaultOption(kAutoNameDefault, kAutoNameDefault);
   m_chooser.AddOption(kAutoNameCustom, kAutoNameCustom);
   frc::SmartDashboard::PutData("Auto Modes", &m_chooser);
+#ifndef CFG_NO_DRIVEBASE
+  m_drivetrain.init();
+  m_odometry.putField2d();
+#endif
 }
 
 /**
@@ -37,6 +41,10 @@ void Robot::RobotPeriodic() {}
  */
 void Robot::AutonomousInit()
 {
+#ifndef CFG_NO_DRIVEBASE
+  m_odometry.update();
+  m_drivetrain.flip();
+#endif
   m_autoSelected = m_chooser.GetSelected();
   // m_autoSelected = SmartDashboard::GetString("Auto Selector",
   //     kAutoNameDefault);
@@ -87,81 +95,102 @@ void Robot::AutonomousPeriodic()
     }
 }
 
-void Robot::TeleopInit() {}
+void Robot::TeleopInit()
+{
+#ifndef CFG_NO_DRIVEBASE
+  m_odometry.update();
+#endif
+}
+
+void Robot::swerveDrive(bool const &field_relative)
+{
+#ifndef CFG_NO_DRIVEBASE
+  const units::meters_per_second_t left_right { -frc::ApplyDeadband(BUTTON::DRIVETRAIN::LX(), CONSTANTS::DEADBAND)};
+
+  const units::meters_per_second_t front_back { -frc::ApplyDeadband(BUTTON::DRIVETRAIN::LY(), CONSTANTS::DEADBAND)};
+  auto const rot = frc::ApplyDeadband(BUTTON::DRIVETRAIN::RX(), CONSTANTS::DEADBAND) * (CONSTANTS::DRIVE::TELEOP_MAX_ANGULAR_SPEED);
+
+  m_drivetrain.drive(front_back, -left_right, rot, field_relative);
+
+  // frc::SmartDashboard::PutNumber("Gyro: ", m_drivetrain.getAngle().value());
+  // frc::SmartDashboard::PutNumber("front/back: ", front_back.value());
+  // frc::SmartDashboard::PutNumber("left/right: ", left_right.value());
+#endif
+}
 
 void Robot::TeleopPeriodic()
 {
   if(BUTTON::STOWED())
-    {
-      m_stowed_toggle = !m_stowed_toggle;
-      m_intake_toggle = 0;
-      m_man_intake_toggle = 0;
-      m_extake_toggle = 0;
-    }
+  {
+    m_stowed_toggle = !m_stowed_toggle;
+    m_intake_toggle = 0;
+    m_man_intake_toggle = 0;
+    m_extake_toggle = 0;
+  }
   if(BUTTON::INTAKE())
-    {
-      m_stowed_toggle = 0;
-      m_intake_toggle = !m_intake_toggle;
-      m_man_intake_toggle = 0;
-      m_extake_toggle = 0;
-    }
+  {
+    m_stowed_toggle = 0;
+    m_intake_toggle = !m_intake_toggle;
+    m_man_intake_toggle = 0;
+    m_extake_toggle = 0;
+  }
   if(BUTTON::MAN_INTAKE())
-    {
-      m_stowed_toggle = 0;
-      m_intake_toggle = 0;
-      m_man_intake_toggle = !m_man_intake_toggle;
-      m_extake_toggle = 0;
-    }
+  {
+    m_stowed_toggle = 0;
+    m_intake_toggle = 0;
+    m_man_intake_toggle = !m_man_intake_toggle;
+    m_extake_toggle = 0;
+  }
   if(BUTTON::EXTAKE())
-    {
-      m_stowed_toggle = 0;
-      m_intake_toggle = 0;
-      m_man_intake_toggle = 0;
-      m_extake_toggle = !m_extake_toggle;
-    }
+  {
+    m_stowed_toggle = 0;
+    m_intake_toggle = 0;
+    m_man_intake_toggle = 0;
+    m_extake_toggle = !m_extake_toggle;
+  }
 
   if(m_extake_toggle)
-    {
-      m_state = CONSTANTS::STATE::EXTAKE;
-    }
+  {
+    m_state = CONSTANTS::STATE::EXTAKE;
+  }
   if(m_intake_toggle)
-    {
-      m_state = CONSTANTS::STATE::INTAKE;
-    }
+  {
+    m_state = CONSTANTS::STATE::INTAKE;
+  }
   if(m_stowed_toggle)
-    {
-      m_state = CONSTANTS::STATE::STOWED;
-    }
+  {
+    m_state = CONSTANTS::STATE::STOWED;
+  }
   if(m_man_intake_toggle)
-    {
-      m_state = CONSTANTS::STATE::MAN_INTAKE;
-    }
+  {
+    m_state = CONSTANTS::STATE::MAN_INTAKE;
+  }
   switch(m_state)
-    {
+  {
     case CONSTANTS::STATE::STOWED:
       m_arm.move(CONSTANTS::ARM::STORE_POS);
       m_roller.spin(CONSTANTS::ROLLER::SLOW);
 
       break;
     case CONSTANTS::STATE::MAN_INTAKE: // NOTE: No specific code needs to be
-                                       // written for man intake. If the state
-                                       // is man intake, is_loaded will always
-                                       // return false. Fallthough inferred
+      // written for man intake. If the state
+      // is man intake, is_loaded will always
+      // return false. Fallthough inferred
     case CONSTANTS::STATE::INTAKE:
       m_arm.move(CONSTANTS::ARM::INTAKE_POS);
       m_roller.spin(1);
 
       if(m_roller.is_loaded())
-        {
-          m_state = CONSTANTS::STATE::STOWED;
-        }
+      {
+        m_state = CONSTANTS::STATE::STOWED;
+      }
       break;
     case CONSTANTS::STATE::EXTAKE:
       m_arm.move(CONSTANTS::ARM::SCORE_POS);
       m_roller.spin(CONSTANTS::FF_SPEED);
       m_roller.spin(0);
       break;
-    }
+  }
 }
 void Robot::DisabledInit() {}
 
