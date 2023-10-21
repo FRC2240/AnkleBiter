6e03688da1f3ebfa958f7aff868d764a32eca85e
William4813 <93utua@gmail.com>
2023-10-21 12:34:35 -0600

    started refactor

---
+++ src/main/include/swerve/Odometry.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include "swerve/SwerveModule.h"
+#include "swerve/Drivetrain.h"
+#include "swerve/ngr.h"
+#include "Constants.h"
+#include <iostream>
+#include <frc/kinematics/SwerveDriveOdometry.h>
+#include <frc/kinematics/SwerveDriveKinematics.h>
+#include <frc/smartdashboard/SmartDashboard.h>
+
+
+#include <frc/kinematics/ChassisSpeeds.h>
+#include <frc/smartdashboard/Field2d.h>
+#include <frc/geometry/Pose2d.h>
+#include <frc/estimator/SwerveDrivePoseEstimator.h>
+//#include <TimeOfFlight.h>
+#include <frc/DriverStation.h>
+
+#ifndef CFG_NO_DRIVEBASE
+class Odometry
+{
+    public:
+        Odometry(Drivetrain* drivetrain);
+        void putField2d();
+
+    [[nodiscard]] frc::Pose2d getPose();
+
+    void reset_position_from_vision(const frc::Pose2d &bot_pose);
+
+    void reset_from_distance();
+
+    void update();
+
+    void resetPosition(const frc::Pose2d &pose,
+                       const frc::Rotation2d &gyroAngle);
+
+    [[nodiscard]] frc::FieldObject2d *getField2dObject(std::string_view name);
+
+    [[nodiscard]] frc::ChassisSpeeds const getFieldRelativeSpeeds();
+    private:
+        Drivetrain* m_drivetrain;
+};
+#endif
---
+++ src/main/include/swerve/SwerveModule.h
@@ -0,0 +1,60 @@
+#pragma once
+
+/// Comment below out if you want to drive
+//#define CFG_NO_DRIVEBASE
+
+#ifndef CFG_NO_DRIVEBASE
+#include <frc/kinematics/SwerveModuleState.h>
+#include <frc/kinematics/SwerveModulePosition.h>
+#include <frc/geometry/Translation2d.h>
+
+#include <ctre/Phoenix.h>
+#include "Constants.h"
+
+
+class SwerveModule
+{
+public:
+    /******************************************************************/
+    /*                  Public Function Declarations                  */
+    /******************************************************************/
+
+    SwerveModule(int const &driver_adr, int const &turner_adr, int const &cancoder_adr, double const &magnet_offset);
+
+    [[nodiscard]] frc::SwerveModuleState getState();
+
+    [[nodiscard]] frc::SwerveModulePosition getPosition();
+
+    [[nodiscard]] units::degree_t getAngle();
+
+    [[nodiscard]] double getDriverTemp();
+
+    [[nodiscard]] double getTurnerTemp();
+
+    double get_current();
+
+    void setDesiredState(const frc::SwerveModuleState &state);
+
+    void percentOutputControl(double const &percent_output);
+
+    void manualVelocityContol(double const &velocity_ticks_per_100ms);
+
+    double getEncoder() {
+        return driver.GetSelectedSensorPosition();
+    }
+
+    // No copies/moves should be occuring (Talons don't support this)
+    SwerveModule(SwerveModule const &) = delete;
+    SwerveModule(SwerveModule &&) = delete;
+
+private:
+    /******************************************************************/
+    /*                        Private Variables                       */
+    /******************************************************************/
+
+    WPI_TalonFX driver, turner;
+    WPI_CANCoder cancoder;
+    double const magnet_offset;
+    int turner_addr;
+};
+#endif
